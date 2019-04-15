2a28419690d833b5d016009d05c7aa0d22a255da
yunjing liu <yl483@duke.edu>
2019-04-15 12:53:12 -0400

    clean

--- .sneaky_mod.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yl483/compsci590-final-project/sneaky_mod.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/yl483/compsci590-final-project/sneaky_mod.ko /home/yl483/compsci590-final-project/sneaky_mod.o /home/yl483/compsci590-final-project/sneaky_mod.mod.o
